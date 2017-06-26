f4e05172f2727f2e8fb95904853a7c87f00127ea
romanvit13 <romanvit13@gmail.com>
2017-06-26 19:43:22 +0300

    Added saving feature.

--- Android/app/src/main/java/com/example/roman/hookup/MainActivity.java
+++ Android/app/src/main/java/com/example/roman/hookup/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.roman.hookup;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,31 +12,52 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final EditText firstNameEdit = (EditText) findViewById(R.id.firstNameEdit);
         final Boolean[] firstNameEditClicked = {false};
+        final String FIRST_NAME_EDIT = "first name";
 
         final EditText lastNameEdit = (EditText) findViewById(R.id.lastNameEdit);
         final Boolean[] lastNameEditClicked = {false};
+        final String LAST_NAME_EDIT = "second name";
 
         final EditText numberEdit = (EditText) findViewById(R.id.phoneNumberEdit);
         final Boolean[] numberEditClicked = {false};
+        final String NUMBER_EDIT = "number";
 
         final EditText facebookEdit = (EditText) findViewById(R.id.facebookEdit);
         final Boolean[] facebookEditClicked = {false};
+        final String FACEBOOK_EDIT = "facebook";
 
         final EditText instaEdit = (EditText) findViewById(R.id.instaEdit);
         final Boolean[] instaEditClicked = {false};
+        final String INSTA_EDIT = "insta";
 
         final EditText vkEdit = (EditText) findViewById(R.id.vkEdit);
         final Boolean[] vkEditClicked = {false};
+        final String VK_EDIT = "vk";
 
         final Button shareButton = (Button) findViewById(R.id.shareToActivityButton);
         final Button saveButton = (Button) findViewById(R.id.saveEditDataButton);
+
+        final Button receiveButton = (Button) findViewById(R.id.receiveButton);
+
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        String firstNameSaved = sharedPref.getString(FIRST_NAME_EDIT, "First name");
+        firstNameEdit.setText(firstNameSaved);
+
+        lastNameEdit.setText(sharedPref.getString(LAST_NAME_EDIT, "Second name"));
+
+        numberEdit.setText(sharedPref.getString(NUMBER_EDIT, "Number"));
+        facebookEdit.setText(sharedPref.getString(FACEBOOK_EDIT, "Facebook login"));
+        instaEdit.setText(sharedPref.getString(INSTA_EDIT, "Instagram login"));
+        vkEdit.setText(sharedPref.getString(VK_EDIT, "VK login"));
 
         firstNameEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,6 +116,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        receiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openReceiveActivity = new Intent(MainActivity.this, ReceiveActivity.class);
+                startActivity(openReceiveActivity);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = MainActivity.this;
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                if (firstNameEdit.getText().toString() != "First Name") {
+                    editor.putString(FIRST_NAME_EDIT, firstNameEdit.getText().toString());
+                }
+                if (lastNameEdit.getText().toString() != "Last Name") {
+                    editor.putString(LAST_NAME_EDIT, lastNameEdit.getText().toString());
+                }
+                if (numberEdit.getText().toString() != "Number") {
+                    editor.putString(NUMBER_EDIT, numberEdit.getText().toString());
+                }
+                if (facebookEdit.getText().toString() != "Facebook login") {
+                    editor.putString(FACEBOOK_EDIT, facebookEdit.getText().toString());
+                }
+                if (instaEdit.getText().toString() != "Instagram login") {
+                    editor.putString(INSTA_EDIT, instaEdit.getText().toString());
+                }
+                if (vkEdit.getText().toString() != "Vk login") {
+                    editor.putString(VK_EDIT, vkEdit.getText().toString());
+                }
+                editor.commit();
+
+                showToast("Your data was saved");
+            }
+        });
+
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,16 +162,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(openShareActivity);
             }
         });
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast("Your data was saved successfully!");
-            }
-        });
     }
 
     private void showToast(String message) {
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
     }
+
+
 }
---
+++ Android/app/src/main/java/com/example/roman/hookup/ReceiveActivity.java
@@ -0,0 +1,28 @@
+package com.example.roman.hookup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ReceiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive);
+
+        Button receiveButton = (Button) findViewById(R.id.receiveButton);
+        receiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Taking data from another phone...");
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(ReceiveActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+}
