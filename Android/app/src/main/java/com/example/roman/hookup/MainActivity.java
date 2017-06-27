3de4197c0e19d55f0b30aeb5b50abb0499b917eb
romanvit13 <romanvit13@gmail.com>
2017-06-27 21:35:15 +0300

    Added gallery pick up feature.

--- Android/app/src/main/java/com/example/roman/hookup/MainActivity.java
+++ Android/app/src/main/java/com/example/roman/hookup/MainActivity.java
@@ -1,122 +1,106 @@
 package com.example.roman.hookup;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText[] textViews;
+    private Map<Integer, Boolean> textViewsClicked;
+    private Uri imageUri;
+    private ImageView cameraView;
+    private int PICK_IMAGE = 123;
+    private Bitmap camImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Lists for Edit Texts*/
+        textViews = new EditText[6];
+        textViewsClicked = new HashMap<>();
 
+        /*Initialize the list*/
+        textViews[0] = (EditText) findViewById(R.id.firstNameEdit);
+        textViews[1] = (EditText) findViewById(R.id.lastNameEdit);
+        textViews[2] = (EditText) findViewById(R.id.phoneNumberEdit);
+        textViews[3] = (EditText) findViewById(R.id.facebookEdit);
+        textViews[4] = (EditText) findViewById(R.id.instaEdit);
+        textViews[5] = (EditText) findViewById(R.id.vkEdit);
 
-        final EditText firstNameEdit = (EditText) findViewById(R.id.firstNameEdit);
-        final Boolean[] firstNameEditClicked = {false};
-        final String FIRST_NAME_EDIT = "first name";
-
-        final EditText lastNameEdit = (EditText) findViewById(R.id.lastNameEdit);
-        final Boolean[] lastNameEditClicked = {false};
-        final String LAST_NAME_EDIT = "second name";
-
-        final EditText numberEdit = (EditText) findViewById(R.id.phoneNumberEdit);
-        final Boolean[] numberEditClicked = {false};
-        final String NUMBER_EDIT = "number";
-
-        final EditText facebookEdit = (EditText) findViewById(R.id.facebookEdit);
-        final Boolean[] facebookEditClicked = {false};
-        final String FACEBOOK_EDIT = "facebook";
-
-        final EditText instaEdit = (EditText) findViewById(R.id.instaEdit);
-        final Boolean[] instaEditClicked = {false};
-        final String INSTA_EDIT = "insta";
-
-        final EditText vkEdit = (EditText) findViewById(R.id.vkEdit);
-        final Boolean[] vkEditClicked = {false};
-        final String VK_EDIT = "vk";
-
-        final Button shareButton = (Button) findViewById(R.id.shareToActivityButton);
+        /*Three functional buttons.*/
         final Button saveButton = (Button) findViewById(R.id.saveEditDataButton);
-
+        final Button shareButton = (Button) findViewById(R.id.shareToActivityButton);
         final Button receiveButton = (Button) findViewById(R.id.receiveButton);
+        cameraView = (ImageView) findViewById(R.id.cameraView);
 
         SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
-        String firstNameSaved = sharedPref.getString(FIRST_NAME_EDIT, "First name");
-        firstNameEdit.setText(firstNameSaved);
+        loadImageFromStorage();
+        //saveData();
 
-        lastNameEdit.setText(sharedPref.getString(LAST_NAME_EDIT, "Second name"));
+        for (int i = 0; i < textViews.length; i++) {
 
-        numberEdit.setText(sharedPref.getString(NUMBER_EDIT, "Number"));
-        facebookEdit.setText(sharedPref.getString(FACEBOOK_EDIT, "Facebook login"));
-        instaEdit.setText(sharedPref.getString(INSTA_EDIT, "Instagram login"));
-        vkEdit.setText(sharedPref.getString(VK_EDIT, "VK login"));
+            String saved = sharedPref.getString(textViews[i].getId() + "", "");
+            if ((textViews[i].getText().toString() != "")
+                    && (textViews[i].getText().toString() != " ")
+                    && (textViews[i].getText().toString() != "  ")) {
+                textViews[i].setText(saved);
+            }
 
-        firstNameEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (firstNameEditClicked[0] == false) {
-                    firstNameEdit.setText("");
-                    firstNameEditClicked[0] = true;
+            textViews[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!(textViewsClicked.keySet().contains(v.getId())
+                            && textViewsClicked.get(v.getId()))) {
+                        ((EditText) v).setText("");
+                        textViewsClicked.put(v.getId(), true);
+                    }
                 }
-            }
-        });
-        lastNameEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (lastNameEditClicked[0] == false) {
-                    lastNameEdit.setText("");
-                    lastNameEditClicked[0] = true;
-                }
-            }
-        });
-        numberEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numberEditClicked[0] == false) {
-                    numberEdit.setText("");
-                    numberEditClicked[0] = true;
-                }
-            }
-        });
+            });
+        }
 
-        facebookEdit.setOnClickListener(new View.OnClickListener() {
+        /*Save the data.*/
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (facebookEditClicked[0] == false) {
-                    facebookEdit.setText("");
-                    facebookEditClicked[0] = true;
-                }
-            }
-        });
-        instaEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (instaEditClicked[0] == false) {
-                    instaEdit.setText("");
-                    instaEditClicked[0] = true;
-                }
-            }
-        });
-
-        vkEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (vkEditClicked[0] == false) {
-                    vkEdit.setText("");
-                    vkEditClicked[0] = true;
-                }
+                saveData();
+                showToast("Your data was saved");
             }
         });
 
 
+
+        /*Share activity.*/
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openShareActivity = new Intent(MainActivity.this, ShareActivity.class);
+                startActivity(openShareActivity);
+            }
+        });
+
+        /*Receive activity.*/
         receiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -125,41 +109,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
+        cameraView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context context = MainActivity.this;
-                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                if (firstNameEdit.getText().toString() != "First Name") {
-                    editor.putString(FIRST_NAME_EDIT, firstNameEdit.getText().toString());
-                }
-                if (lastNameEdit.getText().toString() != "Last Name") {
-                    editor.putString(LAST_NAME_EDIT, lastNameEdit.getText().toString());
-                }
-                if (numberEdit.getText().toString() != "Number") {
-                    editor.putString(NUMBER_EDIT, numberEdit.getText().toString());
-                }
-                if (facebookEdit.getText().toString() != "Facebook login") {
-                    editor.putString(FACEBOOK_EDIT, facebookEdit.getText().toString());
-                }
-                if (instaEdit.getText().toString() != "Instagram login") {
-                    editor.putString(INSTA_EDIT, instaEdit.getText().toString());
-                }
-                if (vkEdit.getText().toString() != "Vk login") {
-                    editor.putString(VK_EDIT, vkEdit.getText().toString());
-                }
-                editor.commit();
 
-                showToast("Your data was saved");
-            }
-        });
-
-        shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent openShareActivity = new Intent(MainActivity.this, ShareActivity.class);
-                startActivity(openShareActivity);
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, PICK_IMAGE);
             }
         });
     }
@@ -168,5 +123,72 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
+    public int saveData() {
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        for (int i = 0; i < textViews.length; i++) {
+            if ((textViews[i].getText().toString() != "")
+                    && (textViews[i].getText().toString() != " ")
+                    && (textViews[i].getText().toString() != "  ")) {
+                editor.putString(textViews[i].getId() + "", textViews[i].getText().toString());
+            }
+        }
+        editor.commit();
+        return 0;
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((resultCode == RESULT_OK) && (requestCode == PICK_IMAGE)) {
+            imageUri = data.getData();
+            cameraView.setImageURI(imageUri);
+            try {
+                camImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                saveImageToInternalStorage(camImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private String saveImageToInternalStorage(Bitmap bitmapImage) {
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+
+        File directory = cw.getExternalFilesDir("imageDir");
+        // Create imageDir
+        File myPath = new File(directory.toString(), "profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myPath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    private void loadImageFromStorage() {
+
+        try {
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+            File directory = cw.getExternalFilesDir("imageDir");
+            File f = new File(directory.toString(), "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            cameraView.setImageBitmap(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
